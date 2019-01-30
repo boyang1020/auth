a90f037600cccea583c09d9a8713cf64e46409d3
boyang1020 <boyang1020@gmail.com>
2019-01-29 19:16:39 -0800

    login form scaffolding

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View } from 'react-native';
 import firebase from 'firebase';
 import { Header } from './components/common';
-
+import LoginForm from './components/LoginForm';
 
 class App extends Component {
     componentWillMount() {
@@ -22,7 +22,7 @@ class App extends Component {
         return (
             <View>
                 <Header headerText="Authentication" />
-                <Text>An app</Text>
+                <LoginForm />
             </View>
         );
     }
