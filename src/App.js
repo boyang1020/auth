c5158542bdaa724bf7e79ff7f02934329600d719
boyang1020 <boyang1020@gmail.com>
2019-01-29 19:02:14 -0800

    firebase client setup

--- src/App.js
+++ src/App.js
@@ -1,14 +1,31 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
+import firebase from 'firebase';
+import { Header } from './components/common';
+
 
 class App extends Component {
+    componentWillMount() {
+        firebase.initializeApp(
+            {
+                apiKey: "AIzaSyBgNzkecYK2lVymY5TYM8h_gCOxhkendJw",
+                authDomain: "auth-e76cb.firebaseapp.com",
+                databaseURL: "https://auth-e76cb.firebaseio.com",
+                projectId: "auth-e76cb",
+                storageBucket: "auth-e76cb.appspot.com",
+                messagingSenderId: "210889096339"
+              }
+        );
+    }
+
     render() {
         return (
             <View>
+                <Header headerText="Authentication" />
                 <Text>An app</Text>
             </View>
         );
     }
 }
 
-export default App;ß
+export default App;
